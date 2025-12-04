98da0728103ca691be62e3b2849a552d648a7d0d
Baha <bahajedidi2016@gmail.com>
2025-12-05 00:17:24 +0100

    Resolve merge conflict in postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   plugins: {
-<<<<<<< HEAD
     'tailwindcss/nesting': {},
     tailwindcss: {},
-=======
-    '@tailwindcss/postcss': {},
->>>>>>> 00464d4ead25df3f8333cbe7d82ccf2e3ede44cc
     autoprefixer: {},
   },
 };
